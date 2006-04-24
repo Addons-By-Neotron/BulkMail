c79168b091b7ba422fbbb9064a876af81ff0108c
hyperactiveChipmunk <hyperactiveChipmunk@437ccee7-0d8d-4378-9327-a87cf3263d26>
2006-04-24 22:39:15 +0000

    BulkMail: - Added stuff for adding, removing, and destination-changing of single items to an autosend list - Added ability to send all items in the autosend list - While I think everything 'works', there is NO interface.  This is not practical for use yet, so no whining.

--- tmp/trunk/BulkMail/BulkMail.lua
+++ tmp/trunk/BulkMail/BulkMail.lua
@@ -1,64 +1,32 @@
--- To use this template, simply replace all occurances of BulkMail and self.loc
--- with the name of your addon. Make sure to use case-sensitive replacement for
--- global search and replacement.
-
--- NOTE: The globals TRUE and FALSE, defined in Ace.lua, contain the values 1 and
--- nil respectively. Their use is not required. The reason they are used as a
--- standard in Ace is just for readability. It's easier at a glance to know that a
--- variable is intended to be a boolean if you see TRUE and FALSE rather than 1 and
--- nil. Why not use true and false? Primarily because of false. false is equivalent
--- to nil but unlike nil it is an actual value that will keep the variable in
--- memory. And all those 'false' strings take up space in the SaveVariables file.
--- This is generally just a minor resource savings, but enough grains of sand make
--- a desert and all that. Use whatever method is your preference.
-
-local DEFAULT_OPTIONS = {
-	opt1 = FALSE,
-	opt2 = TRUE
-}
-
--- This data map may be used to map database values to command arguments, primarily
--- for use in the report() method. This is often necessary for localization. Say a
--- command can be entered as /addon display text. Hopefully display and text are
--- localized, but in your database display might be stored non-localized. So in the
--- map below you might have 'text' mapped to a global self.loc.DISPLAY_TEXT. The
--- [0] index will be used by the report() method as the display value if there is
--- no value. Otherwise, report() will display a generic "no value" message.
-local DATA_MAP = {
-	opt1     = {
-	    [0]  = BulkMailLocals.OPT1_DEFAULT,
-		val1 = BulkMailLocals.OPT1_VAL1,
-		val2 = BulkMailLocals.OPT1_VAL2
-	}
-}
-
---[[--------------------------------------------------------------------------------
-  Class Setup
------------------------------------------------------------------------------------]]
-
--- See the "Ace Usage Guide.txt" document in the development kit for explanations of
--- each of the following parameters.
 BulkMail = AceAddon:new({
 	name            = BulkMailLocals.NAME,
 	description     = BulkMailLocals.DESCRIPTION,
 	version         = "0.1.0",
-	releaseDate     = "04-08-2006",
+	releaseDate     = "04-23-2006",
 	aceCompatible   = "103",
 	author          = "Mynithrosil of Feathermoon",
 	email           = "hyperactiveChipmunk@gmail.com",
 	website         = "http://hyperactiveChipmunk.wowinterface.com",
-	category        = "inventory",
+	category        = "other",
 	db              = AceDatabase:new("BulkMailDB"),
 	defaults        = DEFAULT_OPTIONS,
 	cmd             = AceChatCmd:new(BulkMailLocals.COMMANDS, BulkMailLocals.CMD_OPTIONS),
-    loc             = BulkMailLocals,
+	loc             = BulkMailLocals,
 })
 
 function BulkMail:Initialize()
 	if not self.data then
-    	self.data = {}
-    end
+		self.data = {}
+	end
 
+	if not self.charData then
+		self.charData = {}
+	end
+
+	if not self.charData.autoSendListItems then
+		self.charData.autoSendListItems = {}
+	end
+	
 end
 
 --[[--------------------------------------------------------------------------------
@@ -66,7 +34,7 @@ end
 -----------------------------------------------------------------------------------]]
 
 function BulkMail:Enable()
-	--self:RegisterEvent("SOME_EVENT", "ProcessSomeEvent")
+	self:RegisterEvent("MAIL_SHOW")
 	--self:Hook("SomeFunction", "ProcessSomeFunction")
 	--self:Hook(SomeObject, "SomeMethod", "ProcessSomeObjectMethod")
 	--self:HookScript(SomeFrame, "OnShow", "ProcessOnShow")
@@ -77,12 +45,97 @@ end
 function BulkMail:Disable()
 end
 
+--[[--------------------------------------------------------------------------------
+  Event Processing
+-----------------------------------------------------------------------------------]]
+
+function BulkMail:MAIL_SHOW()
+	OpenAllBags()
+	self:InitializeContainerFrames()
+	
+	local bag, slot, itemID
+	for k, f in self.containerFrames do
+		bag = f:GetParent():GetID()
+		slot = f:GetID()
+		_, _, itemID = string.find(GetContainerItemLink(bag, slot) or "", "item:(%d+):")
+		if not self.charData.autoSendListItems[itemID] then
+			SetItemButtonDesaturated(f, 1)
+		end
+	end
+end
 
 --[[--------------------------------------------------------------------------------
   Main Processing
 -----------------------------------------------------------------------------------]]
 
-function BulkMail:ProcessSomeEvent()
+local function GetItemID(item)
+	if type(item) ~= 'number' then
+		_, _, item = string.find(tostring(item), "item:(%d+):")
+	end
+	return item
+end
+		
+function BulkMail:InitializeContainerFrames() --creates self.containerFrames, a table consisting of all frames which are container buttons
+	local enum = EnumerateFrames
+	local f = enum()
+	self.containerFrames = {}
+	while f do
+		if (f.hasItem or f.SplitStack) and f:GetID() and f:GetID() > 0 then
+			table.insert(self.containerFrames, f)
+		end
+		f = enum(f)
+	end
+end
+
+function BulkMail:AddAutoSendItem(item, destination)
+	local itemID = GetItemID(item)
+	if not self.charData.autoSendListItems[itemID] then
+		print("Adding..."..itemID)
+		self.charData.autoSendListItems[tostring(itemID)] = destination
+	else
+		self.cmd:msg(self.loc.ERROR_ITEM_ALREADY_IN_AUTOSEND_LIST)
+	end
+end
+
+function BulkMail:ChangeAutoSendDestination(item, newDestination)
+	local itemID = GetItemID(item)
+	if self.charData.autoSendListItems[itemID] then
+		self.charData.autoSendListItems[itemID] = destination
+	else
+		self.cmd:msg(self.loc.ERROR_ITEM_NOT_IN_AUTOSEND_LIST)
+	end
+end
+
+function BulkMail:RemoveAutoSendItem(item)
+	local itemID = GetItemID(item)
+	if self.charData.autoSendListItems[itemID] then
+		self.charData.autoSendListItems[itemID] = nil
+	else
+		self.cmd:msg(self.loc.ERROR_ITEM_NOT_IN_AUTOSEND_LIST)
+	end
+end
+
+function BulkMail:RemoveAutoSendDestination(destination)
+	for itemID, dest in self.charData.autoSendListItems do
+		if destination == dest then
+			self.charData.autoSendListItems[itemID] = nil
+		end
+	end
+end
+
+function BulkMail:SendAllItems()
+	local bag, slot, itemID, itemName, itemCount
+	for k, f in self.containerFrames do
+		bag = f:GetParent():GetID()
+		slot = f:GetID()
+		_, _, itemID = string.find(GetContainerItemLink(bag, slot) or "", "item:(%d+):")
+		if self.charData.autoSendListItems[itemID] then
+			SendMailNameEditBox:SetText(self.charData.autoSendListItems[itemID])
+			PickupContainerItem(bag, slot)
+			ClickSendMailItemButton()
+			SendMail(SendMailNameEditBox:GetText(), SendMailSubjectEditBox:GetText(), SendMailBodyEditBox:GetText())
+		end
+	end
 end
 
 --[[--------------------------------------------------------------------------------
--- tmp/trunk/BulkMail/BulkMailLocals.lua
+++ tmp/trunk/BulkMail/BulkMailLocals.lua
@@ -5,5 +5,8 @@ BulkMailLocals = {
     DESCRIPTION = "Bulk mail sending made easy",
     COMMANDS = {"/bm", "/bulkmail"},
     CMD_OPTIONS= {},
+
+	ERROR_ITEM_ALREADY_IN_AUTOSEND_LIST="This item is already in your autosend list.  Please remove it first or use |cff00ffaa/bulkmail autosend change <destination> <item> [item2, ...]|r to change its autosend destination.",
+	ERROR_ITEM_NOT_IN_AUTOSEND_LIST="This item is not currently in your autosend list.  Please use |cff00ffaa/bulkmail autosend add <destination> <item> [item2, ...]|r to add it.",
 }
 end
