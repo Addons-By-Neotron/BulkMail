1ce28dce43eecf2fcb62e5517604e91949578342
hyperactiveChipmunk <hyperactiveChipmunk@437ccee7-0d8d-4378-9327-a87cf3263d26>
2006-07-06 03:23:37 +0000

    BulkMail: - Correct locale file now in the SVN

---
+++ tmp/trunk/BulkMail/BulkMail-enUS.lua
@@ -0,0 +1,20 @@
+function BulkMail_Locale_enUS()
+	return {
+		["Default destination for autosend items is |cffffff78%s|r."] = "Default destination for autosend items is |cffffff78%s|r.",
+		["No default destination set."] = "No default destination set.",
+		["Enter a name in the To: field or set a default destination with |cff00ffaa/bulkmail defaultdest|r."] = "Enter a name in the To: field or set a default destination with |cff00ffaa/bulkmail defaultdest|r.",
+		["WARNING: Cursor item detection is NOT well-defined when multiple items are 'locked'.   Alt-click is recommended for adding items when there is already an item in the Send Mail item frame."] = "WARNING: Cursor item detection is NOT well-defined when multiple items are 'locked'.   Alt-click is recommended for adding items when there is already an item in the Send Mail item frame.",
+		["Item recipient is this character.  Not sending."] = "Item recipient is this character.  Not sending.",
+
+		["This item is already in your autosend list.  Please remove it first or use |cff00ffaa/bulkmail autosend add|r to change its AutoSend destination."] = "This item is already in your autosend list.  Please remove it first or use |cff00ffaa/bulkmail autosend add|r to change its AutoSend destination.",
+		["This item is not currently in your autosend list.  Please use |cff00ffaa/bulkmail autosend add [destination] ITEMLINK [ITEMLINK2, ...]|r to add it."] = "This item is not currently in your autosend list.  Please use |cff00ffaa/bulkmail autosend add [destination] ITEMLINK [ITEMLINK2, ...]|r to add it.",
+		["You must type 'confirm' to clear"] = "You must type 'confirm' to clear",
+		["Please supply a destination for the item(s), or set a default destination with |cff00ffaa/bulkmail defaultdest|r."] = "Please supply a destination for the item(s), or set a default destination with |cff00ffaa/bulkmail defaultdest|r.",
+		
+		["Items to be sent (Alt-Click to add/remove):"] = "Items to be sent (Alt-Click to add/remove):",
+		["No items selected"] = "No items selected",
+		["Clear"] = "Clear",
+		["Send"] = "Send",
+		["Drop items here for Sending"] = "Drop items here for Sending",
+	}
+end
