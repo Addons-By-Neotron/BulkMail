1464d0c64eb21f06940870c536f36120a5e7d7cc
hyperactiveChipmunk <hyperactiveChipmunk@437ccee7-0d8d-4378-9327-a87cf3263d26>
2007-11-12 05:22:39 +0000

    BulkMail2Inbox: 2.3 - Changes for WoW patch 2.3. - Multiple-attached items (and attached cash) will be displayed (and can be taken) independently of the mail in which they are contained. - 'Money' field removed.  Money is displayed in its own row with 'Cash' as the item and the amount is displayed in the 'Qty' field - 'Mail #' field added.  This is the unique mail to which the item belongs. - Alt-delete and Ctrl-return functions apply to ALL items in the same mail, so rows containing the other attachments from the same mail will be also be highlighted when the alt- or ctrl key is held down. - Number of Items added to the header, in addition to number of mails and total cash.

--- tmp/branches/BulkMail2Inbox/2.3/BulkMailInbox.lua
+++ tmp/branches/BulkMail2Inbox/2.3/BulkMailInbox.lua
@@ -6,10 +6,10 @@ BulkMailInbox.L = L
 local tablet = AceLibrary('Tablet-2.0')
 local abacus = AceLibrary('Abacus-2.0')
 
-local _G = getfenv(0)
+local _G = _G
 
-local sortFields, inboxCache, markTable  -- tables
-local ibIndex, inboxCash, cleanPass, cashOnly, markOnly, takeAllInProgress  -- variables
+local sortFields, markTable  -- tables
+local ibIndex, ibAttachIndex, inboxItems, inboxCash, cleanPass, cashOnly, markOnly, takeAllInProgress  -- variables
 
 --[[----------------------------------------------------------------------------
   Table Handling
@@ -43,27 +43,42 @@ end
   Local Processing
 ------------------------------------------------------------------------------]]
 -- Build a table with info about all items and money in the Inbox
-inboxCache = {}
+local inboxCache = {}
 local function inboxCacheBuild()
 	for k in ipairs(inboxCache) do inboxCache[k] = del(inboxCache[k]) end
-	inboxCash = 0
+	inboxCash, inboxItems = 0, 0
 	for i = 1, GetInboxNumItems() do
-		_, _, sender, subject, money, cod, daysLeft, hasItem, _, wasReturned = GetInboxHeaderInfo(i)
-		if hasItem or money > 0 then
+		_, _, sender, subject, money, cod, daysLeft, numItems, _, wasReturned = GetInboxHeaderInfo(i)
+		if money > 0 then
 			table.insert(inboxCache, newHash(
-				'index', i, 'sender', sender, 'bmid', daysLeft..subject, 'returnable', not wasReturned, 'cod', cod,
-				'daysLeft', daysLeft, 'itemLink', GetInboxItemLink(i) or L["Cash Only"], 'money', money, 'qty', select(3, GetInboxItem(i)),
-				'texture', hasItem and select(2, GetInboxItem(i)) or money > 0 and "Interface\\Icons\\INV_Misc_Coin_01"
+				'index', i, 'sender', sender, 'bmid', daysLeft..subject..0, 'returnable', not wasReturned, 'cod', cod,
+				'daysLeft', daysLeft, 'itemLink', L["Cash"], 'money', money, 'texture', "Interface\\Icons\\INV_Misc_Coin_01"
 			))
 			inboxCash = inboxCash + money
+		end
+		if numItems then
+			for j=1, ATTACHMENTS_MAX_SEND do
+				if GetInboxItem(i,j) then
+					table.insert(inboxCache, newHash(
+						'index', i, 'attachment', j, 'sender', sender, 'bmid', daysLeft..subject..j, 'returnable', not wasReturned, 'cod', cod,
+						'daysLeft', daysLeft, 'itemLink', GetInboxItemLink(i,j), 'qty', select(3, GetInboxItem(i,j)), 'texture', (select(2, GetInboxItem(i,j)))
+					))
+					inboxItems = inboxItems + 1
+				end
+			end
 		end
 	end
 	table.sort(inboxCache, function(a,b)
 		local sf = sortFields[BulkMailInbox.db.char.sortField]
 		if a and b then
-			a[sf] = type(a[sf]) == 'boolean' and tostring(a[sf]) or a[sf]
-			b[sf] = type(b[sf]) == 'boolean' and tostring(b[sf]) or b[sf]
-			if a[sf] > b[sf] then return true end
+			a, b = a[sf], b[sf]
+			a = type(a) == "nil" and 0 or type(a) == "boolean" and tostring(a) or a
+			b = type(b) == "nil" and 0 or type(b) == "boolean" and tostring(b) or b
+			if sf == "index" then
+				if a < b then return true end
+			else
+				if a > b then return true end
+			end
 		end
 	end)
 end
@@ -72,8 +87,8 @@ local function takeAll(cash, mark)
 	cashOnly = cash
 	markOnly = mark
 	ibIndex = GetInboxNumItems()
+	ibAttachIndex = 0
 	takeAllInProgress = true
-	invFull = false
 	lastMoneyIndex = 0
 	inboxCacheBuild()
 	BulkMailInbox:MAIL_INBOX_UPDATE()
@@ -96,7 +111,7 @@ function BulkMailInbox:OnInitialize()
 		sortField = 1,
 	})
 
-	sortFields = { 'itemLink', 'qty', 'money', 'returnable', 'sender', 'daysLeft' }
+	sortFields = { 'itemLink', 'qty', 'returnable', 'sender', 'daysLeft', 'index' }
 	markTable = {}
 	inboxCash = 0
 	invFull = false
@@ -106,13 +121,13 @@ function BulkMailInbox:OnInitialize()
 		args = {
 			altdel = {
 				name = L["Alt-click Delete"], type = 'toggle', aliases = L["alt"],
-				desc = L["Enable Alt-Click on inbox items to delete them."],
+				desc = L["Enable Alt-Click on inbox items to delete the mail in which they are contained."],
 				get = function() return self.db.char.altDel end,
 				set = function(v) self.db.char.altDel = v end,
 			},
 			ctrlret = {
 				name = L["Ctrl-click Return"], type = 'toggle', aliases = L["ctrl"],
-				desc = L["Enable Ctrl-click on inbox items to return them."],
+				desc = L["Enable Ctrl-click on inbox items to return the mail in which they are contained."],
 				get = function() return self.db.char.ctrlRet end,
 				set = function(v) self.db.char.ctrlRet = v end,
 			},
@@ -175,6 +190,7 @@ function BulkMailInbox:MAIL_SHOW()
 end
 
 function BulkMailInbox:MAIL_CLOSED()
+	takeAllInProgress = false
 	self:HideInboxGUI()
 	GameTooltip:Hide()
 	self:UnhookAll()
@@ -184,46 +200,54 @@ BulkMailInbox.PLAYER_ENTERING_WORLD = BulkMailInbox.MAIL_CLOSED  -- MAIL_CLOSED 
 function BulkMailInbox:UI_ERROR_MESSAGE(msg)  -- prevent infinite loop when inventory is full
 	if msg == ERR_INV_FULL then
 		invFull = true
-		return self:MAIL_INBOX_UPDATE()
 	end
 end
 
--- Take next inbox item or money; skip past COD items and letters.
-local subject, money, COD, daysLeft, hasItem
-local numItems
+-- Take next inbox item or money; skip past CoD items and letters.
 function BulkMailInbox:MAIL_INBOX_UPDATE()
-	if not takeAllInProgress then return self:ScheduleEvent('BMI_RefreshInboxGUI', self.RefreshInboxGUI, 1, self) end
-	numItems = GetInboxNumItems()
+	if not takeAllInProgress then return self:ScheduleEvent('BMI_RefreshInboxGUI', self.RefreshInboxGUI, .5, self) end
+	local numMails = GetInboxNumItems()
+	cashOnly = cashOnly or invFull
 	if ibIndex <= 0 then
-		if cleanPass or numItems <= 0 or invFull then
+		if cleanPass or numMails <= 0 then
 			takeAllInProgress = false
+			invFull = false
 			return self:RefreshInboxGUI()
 		else
-			ibIndex = numItems
+			ibIndex = numMails
+			ibAttachIndex = 0
 			cleanPass = true
 			return self:ScheduleEvent('BMI_takeAll', takeAll, .1, cashOnly, markOnly)
 		end
 	end
 	
-	subject, money, COD, daysLeft, hasItem = select(4, GetInboxHeaderInfo(ibIndex))
-	ibIndex = ibIndex - 1
-	if markOnly and not markTable[daysLeft..subject] then
+	local curIndex, curAttachIndex = ibIndex, ibAttachIndex
+	local subject, money, cod, daysLeft = select(4, GetInboxHeaderInfo(curIndex))
+
+	if curAttachIndex == ATTACHMENTS_MAX_SEND then
+		ibIndex = ibIndex - 1
+		ibAttachIndex = 0
+	else
+		ibAttachIndex = ibAttachIndex + 1
+	end
+
+	if curAttachIndex > 0 and not GetInboxItem(curIndex, curAttachIndex) or markOnly and not markTable[daysLeft..subject..curAttachIndex] then
 		return self:MAIL_INBOX_UPDATE()
 	end
 
-	if money > 0 then
+	if curAttachIndex == 0 and money > 0 then
 		cleanPass = false
-		if GetInboxInvoiceInfo(ibIndex + 1) then
-			TakeInboxMoney(ibIndex + 1)
+		if GetInboxInvoiceInfo(curIndex) then
+			TakeInboxMoney(curIndex)
 		else
-			TakeInboxMoney(ibIndex + 1)
+			TakeInboxMoney(curIndex)
 			return self:MAIL_INBOX_UPDATE()
 		end
-	end
-
-	if hasItem and not cashOnly and COD == 0 then
+	elseif not cashOnly and cod == 0 then
 		cleanPass = false
-		if not invFull then return TakeInboxItem(ibIndex + 1) end
+		if not invFull then
+			TakeInboxItem(curIndex, curAttachIndex)
+		end
 	end
 	return self:MAIL_INBOX_UPDATE()
 end
@@ -231,30 +255,30 @@ end
 --[[----------------------------------------------------------------------------
   Hooks
 ------------------------------------------------------------------------------]]
-function BulkMailInbox:SetInboxItem(tooltip, index, ...)
+function BulkMailInbox:SetInboxItem(tooltip, index, attachment, ...)
 	if takeAllInProgress then return end
-	local money, COD, _, hasItem, _, wasReturned, _, canReply = select(5, GetInboxHeaderInfo(index))
+	local money, _, _, _, _, wasReturned, _, canReply = select(5, GetInboxHeaderInfo(index))
 	if self.db.char.shiftTake then tooltip:AddLine(L["Shift - Take Item"]) end
 	if wasReturned then 
 		if self.db.char.altDel then
-			tooltip:AddLine(L["Alt - Delete Mail"])
+			tooltip:AddLine(L["Alt - Delete Containing Mail"])
 		end
 	elseif canReply and self.db.char.ctrlRet then
-		tooltip:AddLine(L["Ctrl - Return Item"])
+		tooltip:AddLine(L["Ctrl - Return Containing Mail"])
 	end
 end
 
-function BulkMailInbox:InboxFrame_OnClick(index, ...)
+function BulkMailInbox:InboxFrame_OnClick(index, attachment, ...)
 	takeAllInProgress = false
-	local _, _, _, _, money, COD, _, hasItem, _, wasReturned, _, canReply = GetInboxHeaderInfo(index)
+	local _, _, _, _, money, cod, _, hasItem, _, wasReturned, _, canReply = GetInboxHeaderInfo(index)
  	if self.db.char.shiftTake and IsShiftKeyDown() then
 		if money > 0 then TakeInboxMoney(index)
-		elseif COD > 0 then return
-		elseif hasItem then TakeInboxItem(index) end
+		elseif cod > 0 then return
+		elseif hasItem then TakeInboxItem(index, attachment) end
 	elseif self.db.char.ctrlRet and IsControlKeyDown() and not wasReturned and canReply then ReturnInboxItem(index)
-	elseif self.db.char.altDel and IsAltKeyDown() then DeleteInboxItem(index)
-	else self.hooks.InboxFrame_OnClick(index, ...) end
-	self:ScheduleEvent(self.RefreshInboxGUI, 0.25, self)
+	elseif self.db.char.altDel and IsAltKeyDown() and wasReturned then DeleteInboxItem(index)
+	elseif this:GetObjectType() == 'CheckButton' then self.hooks.InboxFrame_OnClick(index, ...) end
+	self:ScheduleEvent(self.RefreshInboxGUI, 0.1, self)
 end
 
 --[[----------------------------------------------------------------------------
@@ -276,22 +300,42 @@ function BulkMailInbox:UpdateTakeAllButton()
 	end
 end
 
--- Inbox Items GUI
+-- Inbox Items Tablet
+local function highlightSameMailItems(index, ...)
+	if self.db.char.altDel and IsAltKeyDown() or self.db.char.ctrlRet and IsControlKeyDown() then
+		for i = 1, select('#', ...) do
+			row = select(i, ...)
+			if row.col6 and row.col6:GetText() == index then
+				row.highlight:Show()
+			end
+		end
+	end
+end
+
+local function unhighlightSameMailItems(index, ...)
+	for i = 1, select('#', ...) do
+		row = select(i, ...)
+		if row.col6 and row.col6:GetText() == index then
+			row.highlight:Hide()
+		end
+	end
+end
+
 function BulkMailInbox:RegisterInboxGUI()
 	if not self.db.char.inboxUI then return self:HideInboxGUI() end
 	if not tablet:IsRegistered('BMI_InboxTablet') then
 		tablet:Register('BMI_InboxTablet', 'detachedData', self.db.profile.tablet_data, 'strata', "HIGH", 'maxHeight', 850,
 			'cantAttach', true, 'dontHook', true, 'showTitleWhenDetached', true, 'children', function()
-				tablet:SetTitle(string.format(L["BulkMailInbox -- Inbox Items (%d mails, %s)"], GetInboxNumItems(), abacus:FormatMoneyShort(inboxCash)))
+				tablet:SetTitle(string.format(L["BulkMailInbox -- Inbox Items (%d mails, %d items, %s)"], GetInboxNumItems(), inboxItems, abacus:FormatMoneyShort(inboxCash)))
 				local hlcol = 'text'..self.db.char.sortField
 				local cat = tablet:AddCategory('columns', 6,
 					'func', function() self.db.char.sortField = sortFields[self.db.char.sortField+1] and self.db.char.sortField+1 or 1 self:RefreshInboxGUI() end,
-					'text', L["Items (Inbox click actions apply)"],
+					'text',  L["Items (Inbox click actions apply)"],
 					'text2', L["Qty."],
-					'text3', L["Money"],
-					'text4', L["Returnable"],
-					'text5', L["Sender"],
-					'text6', L["TTL"],
+					'text3', L["Returnable"],
+					'text4', L["Sender"],
+					'text5', L["TTL"],
+					'text6', L["Mail #"],
 					hlcol..'R', 1, hlcol..'G', 0.8, hlcol..'B', 0
 				)
 				if inboxCache and next(inboxCache) then
@@ -303,16 +347,16 @@ function BulkMailInbox:RegisterInboxGUI()
 									markTable[info.bmid] = not markTable[info.bmid] and true or nil
 									self:RefreshInboxGUI()
 								else
-									InboxFrame_OnClick(info.index)
+									self:InboxFrame_OnClick(info.index, info.attachment)
 								end
 							end,
 							'onEnterFunc', function()  -- contributed by bigzero
 								GameTooltip:SetOwner(_G.this, 'ANCHOR_RIGHT', 7, -18)
-								GameTooltip:SetInboxItem(info.index)
+								GameTooltip:SetInboxItem(info.index, info.attachment)
 								if IsShiftKeyDown() then
 									GameTooltip_ShowCompareItem()
 								end
-								if info.money > 0 then
+								if info.money then
 									GameTooltip:AddLine(ENCLOSED_MONEY, "", 1, 1, 1)
 									SetTooltipMoney(GameTooltip, info.money)
 									SetMoneyFrameColor('GameTooltipMoneyFrame', HIGHLIGHT_FONT_COLOR.r, HIGHLIGHT_FONT_COLOR.g, HIGHLIGHT_FONT_COLOR.b)
@@ -323,14 +367,18 @@ function BulkMailInbox:RegisterInboxGUI()
 									SetMoneyFrameColor('GameTooltipMoneyFrame', HIGHLIGHT_FONT_COLOR.r, HIGHLIGHT_FONT_COLOR.g, HIGHLIGHT_FONT_COLOR.b)
 								end 
 								GameTooltip:Show()
+								highlightSameMailItems(this.col6 and this.col6:GetText(), this:GetParent():GetChildren())
+							end,
+							'onLeaveFunc', function()
+								unhighlightSameMailItems(this.col6 and this.col6:GetText(), this:GetParent():GetChildren())
 							end,
 							'indentation', markTable[info.bmid] and 0 or 10,
-							'text', info.itemLink,
-							'text2', info.qty,
-							'text3', abacus:FormatMoneyFull(info.money),
-							'text4', info.returnable and L["Yes"] or L["No"],
-							'text5', info.sender,
-							'text6', string.format("%0.1f", info.daysLeft),
+							'text',  info.itemLink or L["Cash"],
+							'text2', info.money and abacus:FormatMoneyFull(info.money) or info.qty,
+							'text3', info.returnable and L["Yes"] or L["No"],
+							'text4', info.sender,
+							'text5', string.format("%0.1f", info.daysLeft),
+							'text6', info.index,
 							'textR', markTable[info.bmid] and 1, 'textG', markTable[info.bmid] and 1, 'textB', markTable[info.bmid] and 1,
 							'text2R', markTable[info.bmid] and 1, 'text2G', markTable[info.bmid] and 1, 'text2B', markTable[info.bmid] and 1,
 							'text3R', markTable[info.bmid] and 1, 'text3G', markTable[info.bmid] and 1, 'text3B', markTable[info.bmid] and 1,
