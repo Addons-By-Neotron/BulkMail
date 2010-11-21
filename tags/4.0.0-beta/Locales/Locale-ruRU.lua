87de9d6844df7b4ad52df7331573d5a339a9b047
dhedbor <dhedbor@437ccee7-0d8d-4378-9327-a87cf3263d26>
2010-11-21 05:34:58 +0000

    Tagging as 4.0.0-beta

---
+++ tags/4.0.0-beta/Locales/Locale-ruRU.lua
@@ -0,0 +1,9 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("BulkMail", "ruRU")
+if not L then return end
+
+-- To help localize BulkMail please enter phrase translations on the
+-- following URL:
+-- http://www.wowace.com/projects/magic-dkp_client/localization/
+-- This file should not be edited manually!
+
+--@localization(locale="ruRU", format="lua_additive_table", handle-subnamespaces="handle-subnamespaces")@
