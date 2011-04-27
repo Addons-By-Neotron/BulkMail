75b0d05e439fc6d9da19d1089a68de0f313e1432
dhedbor <dhedbor@437ccee7-0d8d-4378-9327-a87cf3263d26>
2011-04-27 06:08:18 +0000

    Tagging as 4.0.3

---
+++ tags/4.0.3/Locales/Locale-esMX.lua
@@ -0,0 +1,9 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("BulkMail", "esMX")
+if not L then return end
+
+-- To help localize BulkMail please enter phrase translations on the
+-- following URL:
+-- http://www.wowace.com/projects/bulkmail/localization/
+-- This file should not be edited manually!
+
+--@localization(locale="esMX", format="lua_additive_table", handle-subnamespaces="handle-subnamespaces")@
---
+++ tags/4.0.3/Locales/Locale-zhCN.lua
@@ -0,0 +1,9 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("BulkMail", "zhCN")
+if not L then return end
+
+-- To help localize BulkMail please enter phrase translations on the
+-- following URL:
+-- http://www.wowace.com/projects/bulkmail/localization/
+-- This file should not be edited manually!
+
+--@localization(locale="zhCN", format="lua_additive_table", handle-subnamespaces="handle-subnamespaces")@
